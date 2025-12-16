0d4554b7aa28305ab71e0e742609a626ebd10c9b
BandFlow25 <jason@flowency.co.uk>
2025-12-16 19:09:48 +0000

    Remove WhatWeBuildSection from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "@/components/HeroSection";
 import ProblemSection from "@/components/ProblemSection";
 import ProcessSection from "@/components/ProcessSection";
-import WhatWeBuildSection from "@/components/WhatWeBuildSection";
 import WhyThisWorksSection from "@/components/WhyThisWorksSection";
 import WhoThisIsForSection from "@/components/WhoThisIsForSection";
 import CTASection from "@/components/CTASection";
@@ -12,7 +11,6 @@ const Index = () => {
       <HeroSection />
       <ProblemSection />
       <ProcessSection />
-      <WhatWeBuildSection />
       <WhyThisWorksSection />
       <WhoThisIsForSection />
       <CTASection />
